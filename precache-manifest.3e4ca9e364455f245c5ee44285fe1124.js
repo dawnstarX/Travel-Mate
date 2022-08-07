fcf223d4b01a55f183d538cd272212e2052df833
Abhinav <dawnstar15072000@gmail.com>
2022-08-07 19:43:06 +0530

    Updates

---
+++ precache-manifest.3e4ca9e364455f245c5ee44285fe1124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142dd1a0941062f1acd9858e8ef82889",
+    "url": "/Travel-Mate/index.html"
+  },
+  {
+    "revision": "9a472f5e7b5eedb98fca",
+    "url": "/Travel-Mate/static/css/main.9ee91a79.chunk.css"
+  },
+  {
+    "revision": "d65dd8ad0af16a0aa77a",
+    "url": "/Travel-Mate/static/js/2.1fdbcd2d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Travel-Mate/static/js/2.1fdbcd2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a472f5e7b5eedb98fca",
+    "url": "/Travel-Mate/static/js/main.514261b3.chunk.js"
+  },
+  {
+    "revision": "8a5163191736aef9a64b",
+    "url": "/Travel-Mate/static/js/runtime-main.3f256add.js"
+  }
+]);
