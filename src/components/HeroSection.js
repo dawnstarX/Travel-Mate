f2ca14e78afe93899f70932f5b6ec9714c7f23e2
Abhinav <dawnstar15072000@gmail.com>
2022-08-07 23:06:05 +0530

    final

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
-
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import v1 from "../videos/video-1.mp4";
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+    <div className="hero-container">
+      <video width="750" height="500" autoPlay muted loop>
+        <source src={v1} type="video/mp4" />
+      </video>
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           GET STARTED
         </Button>
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
         >
-          WATCH TRAILER <i className='far fa-play-circle' />
+          WATCH TRAILER <i className="far fa-play-circle" />
         </Button>
       </div>
     </div>
